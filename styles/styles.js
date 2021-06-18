b08070d6db68e8772493ef6eed88c16308f4b91d
syahrizan <syahrizan@rbtsb.com>
2021-06-18 18:49:05 +0800

    added styles file

---
+++ styles/styles.js
@@ -0,0 +1,14 @@
+
+import { Text, View, StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  header: {
+    flex: 1,
+    flexDirection: "row",
+    marginHorizontal: 10,
+    borderBottomWidth: 1,
+    borderColor: "grey",
+  },
+});
+
+export default styles;
