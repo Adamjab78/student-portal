59ba57545a8fc66176d25ea28ba7f8de2f8fb732
syahrizan <syahrizan@rbtsb.com>
2021-06-18 18:48:53 +0800

    Added Schedule component file

---
+++ components/Schedule.js
@@ -0,0 +1,91 @@
+import React from "react";
+import { Text, View} from "react-native";
+
+const Schedule = () => {
+  return (
+    <View style={{ margin: 10 }}>
+      <Text style={{ marginVertical: 10, fontSize: 17, fontWeight: "bold" }}>
+        Schedule
+      </Text>
+      <View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <Text>Hari</Text>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Pagi</Text>
+            </View>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Petang</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <Text>Hari</Text>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Pagi</Text>
+            </View>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Petang</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <Text>Hari</Text>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Pagi</Text>
+            </View>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Petang</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <Text>Hari</Text>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Pagi</Text>
+            </View>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Petang</Text>
+            </View>
+          </View>
+        </View>
+        <View style={{ flex: 1, flexDirection: "row" }}>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <Text>Hari</Text>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Pagi</Text>
+            </View>
+          </View>
+          <View style={{ flex: 1, padding: 5, margin: 5 }}>
+            <View>
+              <Text>Petang</Text>
+            </View>
+          </View>
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Schedule;
