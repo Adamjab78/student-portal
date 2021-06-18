218a863f06c31afcca011b3c2fad2562c60cc617
syahrizan <syahrizan@rbtsb.com>
2021-06-18 18:01:17 +0800

    Create schedule component - one line

--- App.js
+++ App.js
@@ -55,6 +55,33 @@ const Profile = () => {
   );
 };
 
+const Schedule = () => {
+  return (
+    <View style={{ margin: 10 }}>
+      <Text style={{ marginVertical: 10, fontSize: 17, fontWeight: "bold" }}>
+        Schedule
+      </Text>
+      <View>
+      <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, padding: 5, margin: 5 }}>
+        <Text>Hari</Text>
+      </View>
+      <View style={{ flex: 1, padding: 5, margin: 5 }}>
+        <View>
+          <Text>Pagi</Text>
+        </View>
+      </View>
+      <View style={{ flex: 1, padding: 5, margin: 5 }}>
+        <View>
+          <Text>Petang</Text>
+        </View>
+      </View>
+    </View>
+      </View>
+    </View>
+  );
+};
+
 const styles = StyleSheet.create({
   header: {
     flex: 1,
@@ -65,4 +92,4 @@ const styles = StyleSheet.create({
   },
 });
 
-export default App;
+export default Schedule;
