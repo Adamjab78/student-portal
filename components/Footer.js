a09b0bebfe365059c5d08c1308f7d6877a098351
syahrizan <syahrizan@rbtsb.com>
2021-06-26 16:01:49 +0800

    Enhance footer

--- components/Footer.js
+++ components/Footer.js
@@ -1,18 +1,28 @@
 import React from "react";
-import { Text, View, TextInput, Switch } from "react-native";
+import { View} from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 
 const Footer = () => {
   return (
-    <View style={{ flex: 1,flexDirection:"row" }}>
-      <View style={{ flex: 1, borderWidth:1,borderColor:"lightgrey", justifyContent:"center",alignItems:"center" }}>
-        <Ionicons name="home" size={32} />
-      </View>
-      <View style={{ flex: 1, borderWidth:1,borderColor:"lightgrey", justifyContent:"center",alignItems:"center" }}>
-        <Ionicons name="person" size={32} />
-      </View>
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <FooterButton name={"home"} />
+      <FooterButton name={"person"} />
     </View>
   );
+};
+
+const FooterButton = ({name}) => {
+  <View
+    style={{
+      flex: 1,
+      borderWidth: 1,
+      borderColor: "lightgrey",
+      justifyContent: "center",
+      alignItems: "center",
+    }}
+  >
+    <Ionicons name={name} size={32} />
+  </View>;
 };
 
 export default Footer;
