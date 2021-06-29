fa4cbf01d02c91b60de28686f0d97fd77a95010f
syahrizan <syahrizan@rbtsb.com>
2021-06-29 08:58:53 +0800

    Update schedule to receive dynamic data

--- src/components/Schedule.js
+++ src/components/Schedule.js
@@ -1,104 +1,42 @@
 import React from "react";
-import { Text, View } from "react-native";
+import { Text, View, TouchableOpacity } from "react-native";
 
-const Schedule = () => {
+const Schedule = ({ goToCourses, schedule }) => {
   return (
     <View style={{ margin: 10 }}>
       <Text style={{ marginVertical: 10, fontSize: 17, fontWeight: "bold" }}>
         Schedule
       </Text>
       <View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View />
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text style={{ fontWeight: "bold" }}>Pagi</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text style={{ fontWeight: "bold" }}>Petang</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Isnin</Text>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 1</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 2</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Selasa</Text>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 3</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 4</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Rabu</Text>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 5</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 6</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Khamis</Text>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 7</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 8</Text>
-            </View>
-          </View>
-        </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <Text style={{ fontStyle: "italic" }}>Jumaat</Text>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 9</Text>
-            </View>
-          </View>
-          <View style={{ flex: 1, padding: 5, margin: 5 }}>
-            <View>
-              <Text>Subjek 10</Text>
-            </View>
-          </View>
+        {schedule.map((x, i) => (
+          <DailySchedule key={i} goToCourses={goToCourses} item={x} />
+        ))}
+      </View>
+    </View>
+  );
+};
+
+const DailySchedule = ({ item, goToCourses }) => {
+  return (
+    <View style={{ flex: 1, flexDirection: "row" }}>
+      <View style={{ flex: 1, padding: 5, margin: 5 }}>
+        <View>
+          <Text style={{ fontWeight: "bold" }}>{item.day}</Text>
         </View>
       </View>
+      {item.schedule.map((x, i) => (
+        <TouchableOpacity
+          key={i}
+          onPress={() => goToCourses({day:item.day,session:x.session})}
+          style={{ flex: 1, padding: 5, margin: 5 }}
+        >
+          <View>
+            <Text style={{ fontWeight: "bold" }}>
+              {x.courseName || "Vacant"}
+            </Text>
+          </View>
+        </TouchableOpacity>
+      ))}
     </View>
   );
 };
---
+++ src/data/initialScheduleData.js
@@ -0,0 +1,24 @@
+const initialScheduleData = [
+    {
+      day: "Isnin",
+      schedule: [{ session: "Pagi" }, { session: "Petang" }],
+    },
+    {
+      day: "Selasa",
+      schedule: [{ session: "Pagi" }, { session: "Petang" }],
+    },
+    {
+      day: "Rabu",
+      schedule: [{ session: "Pagi" }, { session: "Petang" }],
+    },
+    {
+      day: "Khamis",
+      schedule: [{ session: "Pagi" }, { session: "Petang" }],
+    },
+    {
+      day: "Jumaat",
+      schedule: [{ session: "Pagi" }, { session: "Petang" }],
+    },
+  ];
+
+  export default initialScheduleData
