d0f38364da1c4f116e65a76b5a30f54d067262c1
syahrizan <syahrizan@rbtsb.com>
2021-07-08 21:00:00 +0800

    fix function to add course

--- src/screens/CoursesScreen.js
+++ src/screens/CoursesScreen.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState,useEffect} from "react";
 import {
   Text,
   View,
@@ -15,9 +15,42 @@ import initialScheduleData from "../data/initialScheduleData";
 
 const courses = [
   { title: "English", description: "Modern English", lecturer: "John Doe" },
+  { title: "Math", description: "Modern Math", lecturer: "Max Power" },
+  { title: "Geography", description: "Modern Geography", lecturer: "Max Power" },
 ];
 
 const CoursesScreen = ({ route, navigation }) => {
+
+  const getData = async () => {
+    try {
+      const jsonValue = await AsyncStorage.getItem("schedule");
+      console.log("JSONValue", jsonValue);
+      return jsonValue != null ? JSON.parse(jsonValue) : null;
+    } catch (e) {
+      // error reading value
+      console.log(e);
+    }
+  };
+
+  const populateData = async () => {
+    const scheduleData = await getData();
+    if (scheduleData) {
+      console.log("ada data", scheduleData);
+      setSchedule(scheduleData);
+    } else {
+      //setData(initialScheduleData);
+      console.log("tiada data");
+      setSchedule(initialScheduleData);
+    }
+  };
+
+  const [schedule, setSchedule] = useState(null);
+
+  useEffect(() => {
+    populateData();
+  }, []);
+
+
   const setData = async (value) => {
     try {
       const jsonValue = JSON.stringify(value);
@@ -33,18 +66,20 @@ const CoursesScreen = ({ route, navigation }) => {
     console.log(JSON.stringify(item));
     console.log(x);
 
+    const itemOri=schedule.find(x=>x.day===item.day)
+    console.log("itemOri",itemOri)
     const itemToAdd = {
       day: item.day,
       schedule: [
-        { session: "Pagi", courseName: item.session === "Pagi" ? x : null },
+        { session: "Pagi", courseName: item.session === "Pagi" ? x : (itemOri.schedule.find(y=>y.session==="Pagi")).courseName },
         {
           session: "Petang",
-          courseName: item.session === "Petang" ? x : null,
+          courseName: item.session === "Petang" ? x : (itemOri.schedule.find(y=>y.session==="Petang")).courseName ,
         },
       ],
     };
 
-    const newScheduleData = initialScheduleData;
+    const newScheduleData = schedule;
 
     const index = newScheduleData.indexOf(
       newScheduleData.find((x) => x.day === item.day)
@@ -56,6 +91,8 @@ const CoursesScreen = ({ route, navigation }) => {
     console.log(JSON.stringify(newScheduleData));
 
     setData(newScheduleData);
+
+    navigation.goBack()
 
   };
 
@@ -94,14 +131,16 @@ const CoursesScreen = ({ route, navigation }) => {
           <FlatList
             data={courses}
             keyExtractor={(item, index) => index.toString()}
-            ListHeaderComponent={
-              <View style={{ flex: 1, flexDirection: "row" }}>
-                <Text>Title</Text>
-              </View>
-            }
+            // ListHeaderComponent={
+            //   <View style={{ flex: 1, flexDirection: "row",borderWidth:1 }}>
+            //     <Text>Title</Text>
+            //   </View>
+            // }
             renderItem={({ item, index }) => (
-              <TouchableOpacity onPress={() => addCourse(item.title)}>
+              <TouchableOpacity
+              style={{borderWidth:1,padding:10,marginBottom:10, borderRadius:10,borderColor:"lightgrey"}} onPress={() => addCourse(item.title)}>
                 <Text>{item.title}</Text>
+                <Text>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</Text>
               </TouchableOpacity>
             )}
           />
